08659f202432b8013bb8ca7e5771be82df95568f
Neeraj Gangwar <y.neeraj2008@gmail.com>
2013-10-20 10:14:32 +0530

    Code to get all the data from DB written All codes linked now. Just run GraphSearchIndexingMain to run the indexing.

--- java/src/graphsearch/indexing/GraphSearchTest.java
+++ java/src/graphsearch/indexing/GraphSearchTest.java
@@ -1,9 +1,11 @@
 package graphsearch.indexing;
 
 import graphsearch.searching.GraphSearchSearching;
+import graphsearch.utils.DBQuery;
 import graphsearch.utils.GraphSearchDocument;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,29 +20,38 @@ public class GraphSearchTest
 	 * @param args
 	 * @throws IOException 
 	 * @throws ParseException 
+	 * @throws SQLException 
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) throws IOException, ParseException 
+	public static void main(String[] args) throws IOException, ParseException, ClassNotFoundException, SQLException 
 	{
 		// TODO Auto-generated method stub
-		List<Map<String, String>> list = new ArrayList<Map<String, String>>();
-		GraphSearchIndexing index = new GraphSearchIndexing();
-		GraphSearchSearching search = new GraphSearchSearching();
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("movie", "Fight Club");
-		map.put("actors", "Brad Pitt");
-		map.put("description", "ight Club is a 1999 American drama film based on the 1996 novel of the same name by Chuck Palahniuk. The film was directed by David Fincher and stars Edward Norton, Brad Pitt, and Helena Bonham Carter.");
-		list.add(map);
-		map.clear();
-		map.put("movie", "The Shawshank Redemption");
-		map.put("actors", "Morgan Freeman");
-		map.put("description", "The Shawshank Redemption is a 1994 American drama film written and directed by Frank Darabont and starring Tim Robbins and Morgan Freeman.");
-		list.add(map);
+		/*List<Map<String, String>> list = new ArrayList<Map<String, String>>();
+		GraphSearchIndexing index = new GraphSearchIndexing();		
+		Map<String, String> map1 = new HashMap<String, String>();
+		map1.put("movie", "Fight Club");
+		map1.put("actors", "Brad Pitt");
+		map1.put("description", "Fight Club is a 1999 American drama film based on the 1996 novel of the same name by Chuck Palahniuk. The film was directed by David Fincher and stars Edward Norton, Brad Pitt, and Helena Bonham Carter.");
+		list.add(map1);
+		Map<String, String> map2 = new HashMap<String, String>();
+		map2.put("movie", "The Shawshank Redemption");
+		map2.put("actors", "Morgan Freeman");
+		map2.put("description", "The Shawshank Redemption is a 1994 American drama film written and directed by Frank Darabont and starring Tim Robbins and Morgan Freeman.");
+		list.add(map2);
 		for(int i = 0; i < list.size(); i++)
 		{
 			Map<String, String> currentMovie = list.get(i);
 			GraphSearchDocument currentMovieDoc = new GraphSearchDocument(currentMovie);
 			index.indexDocument(currentMovieDoc);
 		}
-		System.out.println(search.search("freeman", "actors").toString());
+		GraphSearchSearching search = new GraphSearchSearching();
+		System.out.println(search.search("brad", "actors").toString());*/
+		DBQuery db = new DBQuery();
+		List<Map<String, String>> list = db.getFromSQL();
+		System.out.println("\n" + list.size());
+		/*for(int i = 0; i < 10; i++)
+		{
+			System.out.println("\n" + list.get(i));
+		}*/
 	}
 }
--- java/src/graphsearch/utils/DBQuery.java
+++ java/src/graphsearch/utils/DBQuery.java
@@ -1,12 +1,12 @@
 package graphsearch.utils;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,8 +14,7 @@ public class DBQuery
 {
 	private Connection connect = null;
 	private Statement statement = null;
-    //private PreparedStatement preparedStatement = null;
-    //private ResultSet resultSet = null;
+    private ResultSet resultSet = null;
 	
     
 	/* function to get all the data from the database. Function returns of list of maps.
@@ -28,8 +27,17 @@ public class DBQuery
 	    connect = DriverManager.getConnection("jdbc:mysql://172.17.10.167/?"+ "user=neeraj&password=maverick");  // link to database pending
 	    statement = connect.createStatement();
 	    System.out.print(statement.toString());
-	    //resultSet = statement.executeQuery("select * from imdb_actors.*");
-	    //String result = "";
+	    resultSet = statement.executeQuery("select * from testdb.Actors");
+	    System.out.println("\n" + resultSet.getFetchSize());
+	    while( resultSet.next() ) 
+	    {
+	    	Map<String, String> tempMap = new HashMap<String, String>();
+	    	tempMap.put("Name", resultSet.getString("Name").replaceAll(",", ""));
+	    	tempMap.put("Movie", resultSet.getString("Movie"));
+	    	tempMap.put("Year", resultSet.getString("Year"));
+	    	tempMap.put("Role", resultSet.getString("Role"));
+	    	list.add(tempMap);
+	    }
 	    return list;
 	}
 
