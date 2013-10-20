41fb94d2ce98994bbd5daaca36f4c3d65fe71e93
Neeraj Gangwar <y.neeraj2008@gmail.com>
2013-10-20 07:34:28 +0530

    searching code complete a funtion to return a map of results and score is complete.

--- java/src/graphsearch/searching/GraphSearchSearching.java
+++ java/src/graphsearch/searching/GraphSearchSearching.java
@@ -2,16 +2,15 @@ package graphsearch.searching;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
 import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.document.Document;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -23,8 +22,12 @@ public class GraphSearchSearching
 	private Directory indexDirectory = FSDirectory.open(new File(indexLocation));
 	private IndexSearcher indexSearcher = new IndexSearcher(indexDirectory);
 	
+	// constructor
 	public GraphSearchSearching() throws IOException {}
 	
+	/* function to return results of a search query in the form of a map
+	 * it takes field as input and query string as input.
+	 */
 	public Map<Float, String> search(String queryStr, String field) throws ParseException, IOException
 	{
 		Map<Float, String> map = new HashMap<Float, String>();
@@ -32,6 +35,13 @@ public class GraphSearchSearching
 		Query query = parser.parse(queryStr);
 		TopDocs hits = indexSearcher.search(query, 20);
 		
+		for(int i = 0; i < hits.scoreDocs.length; i++)
+		{
+			ScoreDoc scoreDoc = hits.scoreDocs[i];
+			Document doc = indexSearcher.doc(scoreDoc.doc);
+			map.put(scoreDoc.score, doc.get("movie"));
+			
+		}
 		return map;
 	}
 }
